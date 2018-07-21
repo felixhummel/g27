ee95d1dd7779495aadd2a509384da97e542acfb6
Felix Hummel <felix@felixhummel.de>
2018-07-22 00:04:44 +0200

    more cleanup

--- g27.py
+++ g27.py
@@ -47,44 +47,6 @@ def _normalize(x):
         return (x + 32768) / 65535
 
 
-def powergenerator(start=0):
-    """Generate powers of 256"""
-    i = start
-    while True:
-        yield 256 ** i
-        i += 1
-
-
-class Bytewurst(object):
-
-    def __init__(self, bs):
-        self.raw = bs
-        self.ints = [x for x in bs]
-
-    def __repr__(self):
-        return ' '.join(map(hexlify, self.raw))
-
-    @property
-    def int(self):
-        r"""
-        For "01 00 03 0A" ints would be [1, 0, 3, 10], so::
-
-            >>> bs = '\x01\x00\x03\x0A'
-            >>> bw = Bytewurst(bs)
-            >>> bw.int == (1 * 1) + (0 * 256) + (3 * 65536) + (10 * 16777216)
-            True
-        """
-        return sum(a * b for a, b in zip(self.ints, powergenerator()))
-
-    @property
-    def hex(self):
-        return hexlify(self.raw)
-
-    @property
-    def bits(self):
-        return ' '.join([format(x, '08b') for x in self.ints])
-
-
 BUTTON2NAME = """
 0200=wheel axis
 0105=paddle left
@@ -120,13 +82,51 @@ def f():
     for line in BUTTON2NAME.strip().split('\n'):
         a, b = line.split('=')
         yield a.encode('ascii'), b
-button2namedict = dict(f())
+BUTTON2NAME_DICT = dict(f())
+
+
+def powergenerator(start=0):
+    """Generate powers of 256"""
+    i = start
+    while True:
+        yield 256 ** i
+        i += 1
+
+
+class Bytewurst(object):
+
+    def __init__(self, bs):
+        self.raw = bs
+        self.ints = [x for x in bs]
+
+    def __repr__(self):
+        return ' '.join(map(hexlify, self.raw))
+
+    @property
+    def bits(self):
+        return ' '.join(f'{x:08b}' for x in self.ints)
+
+    @property
+    def hex(self):
+        return hexlify(self.raw)
+
+    @property
+    def int(self):
+        """
+        For "01 00 03 0A" ints would be [1, 0, 3, 10], so::
+
+            >>> bs = '\x01\x00\x03\x0A'
+            >>> bw = Bytewurst(bs)
+            >>> bw.int == (1 * 1) + (0 * 256) + (3 * 65536) + (10 * 16777216)
+            True
+        """
+        return sum(a * b for a, b in zip(self.ints, powergenerator()))
 
 
 class Button(Bytewurst):
-    def __init__(self, bs):
-        super().__init__(bs)
-        self.name = button2namedict.get(self.hex, 'UNKNOWN: %s' % self.hex)
+    @property
+    def name(self):
+        return BUTTON2NAME_DICT.get(self.hex, f'UNKNOWN: {self.hex}')
 
 
 class Value(Bytewurst):
@@ -136,8 +136,7 @@ class Value(Bytewurst):
         elif self.int == 1:
             return '   on'
         else:
-            print(self.hex)
-            return '%5d' % self.int
+            return f'{self.int:5d}'
 
     @property
     def normalized(self):
@@ -146,57 +145,51 @@ class Value(Bytewurst):
 
 class Message(object):
 
-    FMT_HEX = '%02X'
-    FMT_DEC = '%03d'
-
     def __init__(self, bs):
         self.bs = bs
-        self.raw_seq = bs[0:4]
-        self.raw_value = bs[4:6]
-        self.raw_id = bs[6:8]
         self.ints = [x for x in bs]
         self.sequence = Bytewurst(bs[0:4])
         self.value = Value(bs[4:6])
         self.button = Button(bs[6:8])
 
     def __repr__(self):
-        return f'{self.sequence.int} {self.button.name:>8} {self.value.normalized:5.0%}'
+        return f'{self.sequence.int} {self.value.normalized:5.0%} {self.button.name}'
+
+    @property
+    def bits(self):
+        return ' '.join([self.sequence.bits, self.value.bits, self.button.bits])
 
     @property
     def hex(self):
         """
         Human-readable hex format. LITTLE ENDIAN!
         """
-        return ' '.join(self.FMT_HEX % x for x in self.ints)
-
-    @property
-    def bit(self):
-        return ' '.join([self.sequence.bits, self.value.bits, self.button.bits])
+        return ' '.join(f'{x:02X}' for x in self.ints)
 
     @property
     def grouped_hex(self):
-        return f'{self.sequence.int:06x} {self.value.int:04x} {self.button.int:04x}'
+        return f'{self.sequence.int:06X} {self.value.int:04X} {self.button.int:04X}'
 
     @property
     def dec(self):
         """Human-readable decimal format"""
-        return ' '.join(self.FMT_DEC % x for x in self.ints)
+        return ' '.join(f'{x:03d}' for x in self.ints)
 
 
-def loop(handler):
+def loop(*handlers):
     with open('/dev/input/js0', 'rb') as device:
         while True:
             bs = device.read(8)
-            handler(bs)
-
-
-def print_message(bs):
-    message = Message(bs)
-    print(message)
+            for handler in handlers:
+                handler(bs)
 
 
 if __name__ == '__main__':
-    loop(print_message)
-    # loop(print)
-    # loop(lambda x: print(Message(x).grouped_hex))
-    # loop(lambda x: print(Message(x).hex))
+    loop(
+        lambda bs: print(Message(bs)),
+        # print,
+        # lambda bs: print(Message(bs).bits),
+        # lambda bs: print(Message(bs).hex),
+        # lambda bs: print(Message(bs).dec),
+        # lambda bs: print(Message(bs).grouped_hex),
+    )
